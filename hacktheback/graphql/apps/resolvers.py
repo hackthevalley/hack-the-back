14854deb9607b6b1fb1c1245c947158907d377db
Alvin Tang <alvin.tang@mail.utoronto.ca>
2021-08-11 00:22:44 -0400

    :fire: Remove unnecessary apps folder in graphql dir

--- hacktheback/graphql/apps/resolvers.py
+++
@@ -1,8 +0,0 @@
-example_data = [
-    {"id": 1, "title": "Form One", "description": "This is form one."},
-    {"id": 2, "title": "Form Two", "description": "This is form two."},
-]
-
-
-def resolve_forms(info):
-    return example_data
