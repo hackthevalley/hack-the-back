a44baa7a0c6fe2c2f1f31dbba0eff54e25bf5a77
Vincent Li <45883132+Cptmoomoo@users.noreply.github.com>
2023-08-03 13:45:49 -0400

    Revert "Revert "Revert "[RELEASE] Update phone number validation"""

--- hacktheback/rest/forms/serializers/form_response.py
+++ hacktheback/rest/forms/serializers/form_response.py
@@ -132,14 +132,9 @@ class AnswerSerializer(serializers.ModelSerializer, ValidationMixin):
         # Validate if the `answer` field is in the correct format
         if question.type == Question.QuestionType.PHONE:
             try:
-                pnUs = phonenumbers.parse(data.get("answer"), "US")
-                if not phonenumbers.is_valid_number(pnUs):
-                    try: 
-                        pn = phonenumbers.parse(data.get("answer"))
-                        if not phonenumbers.is_valid_number(pn):
-                            self.fail_for_field("invalid_phone_number")
-                    except phonenumbers.NumberParseException:
-                        self.fail_for_field("invalid_phone_number")
+                pn = phonenumbers.parse(data.get("answer"))
+                if not phonenumbers.is_valid_number(pn):
+                    self.fail_for_field("invalid_phone_number")
             except phonenumbers.NumberParseException:
                 self.fail_for_field("invalid_phone_number")
         elif question.type == Question.QuestionType.EMAIL:
