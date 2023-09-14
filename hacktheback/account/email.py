4dc970b6739da5bc7a637f75ce70b691092069a6
Vincent Li <vinchenzoli3@gmail.com>
2023-09-13 22:10:30 -0400

    remove comment

--- hacktheback/account/email.py
+++ hacktheback/account/email.py
@@ -81,14 +81,5 @@ class UsernameResetEmail(BaseEmailMessage):
 class RSVPEmail(BaseEmailMessage):
     template_name = "email/rsvp.html"
 
-    # def get_context_data(self):
-    #     # ActivationEmail can be deleted
-    #     context = super().get_context_data()
-
-    #     user = context.get("user")
-    #     context["uid"] = utils.encode_uid(user.pk)
-    #     context["token"] = default_token_generator.make_token(user)
-    #     context["url"] = settings.ACTIVATION_URL.format(**context)
-    #     return context
 
     
