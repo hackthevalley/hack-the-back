49a715af144fa76c4fc34484bb662daf2cd860f3
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-08-20 18:33:35 -0400

    WIP walk-in changes

--- hacktheback/rest/forms/views/walkin.py
+++ hacktheback/rest/forms/views/walkin.py
@@ -5,7 +5,9 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from hacktheback.account.models import User
-from hacktheback.forms.models import HackathonApplicant
+from hacktheback.forms.models import Form, FormResponse, HackathonApplicant
+from hacktheback.rest.forms.serializers import \
+    HackerApplicationResponseSerializer
 from hacktheback.rest.permissions import AdminSiteModelPermissions
 
 from ....forms.utils import send_rsvp_email
@@ -19,13 +21,32 @@ class WalkInAdmissionAPIView(generics.GenericAPIView):
         assert "email" in request.data
         email = request.data.get("email")
         try:
-            applicant = HackathonApplicant.objects.get(application__user__email=email)
-        except HackathonApplicant.DoesNotExist as not_found:
-            raise NotFound(detail="Applicant does not exist") from not_found
-        try:
             user = User.objects.get(email=email)
         except User.DoesNotExist as not_found:
-            raise NotFound(detail="Applicant does not exist") from not_found
+            raise NotFound(detail="User does not exist") from not_found
+        try:
+            applicant = HackathonApplicant.objects.get(application__user__email=email)
+        except HackathonApplicant.DoesNotExist as not_found:
+            print("person got no form, we make now")
+            form = Form.objects.filter(type=Form.FormType.HACKER_APPLICATION, is_draft=False).first()
+            if not form:
+                raise NotFound(detail="No open hacker application form available")
+            print(form.id)
+            form_response_data = {
+                # 'user': user.id,
+                'form': str(form.id),
+                'is_draft': True,  # Assuming you want to create a submitted application
+                'answer': [],
+            }
+            print("we done form_response data");
+            print(form_response_data)
+            serializer = HackerApplicationResponseSerializer(context={'request': request, 'format': None, 'view': self}, data=form_response_data)
+            print("made serializer")
+            print(serializer)
+            serializer.is_valid(raise_exception=True)
+            form_response = serializer.save()
+            print(serializer.data)
+            print("created a form via walkin")
         if applicant.status in [
             HackathonApplicant.Status.ACCEPTED_INVITE,
             HackathonApplicant.Status.ACCEPTED,
