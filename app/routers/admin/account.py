a4aa6559517a92d6299fdae4cff481dde4d678fc
Lilsofie <123223306+Lilsofie@users.noreply.github.com>
2025-09-03 00:17:20 -0400

    Update account.py

--- app/routers/admin/account.py
+++ app/routers/admin/account.py
@@ -66,8 +66,8 @@ async def get_resume(
 
 
 @router.get("/getapplication")
-async def getapplication(uid: UIDRequest, session: SessionDep):
-    statement = select(Account_User).where(Account_User.uid == uid.uid)
+async def getapplication(uid: str, session: SessionDep):
+    statement = select(Account_User).where(Account_User.uid == uid)
     selected_user = session.exec(statement).first()
     if selected_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -77,7 +77,7 @@ async def getapplication(uid: UIDRequest, session: SessionDep):
     return {
         "application": application,
         "form_answers": application.form_answers,
-        "form_answersfile": application.form_answersfile.original_filename
+        "form_answersfile": application.form_answersfile
         if application.form_answersfile
         else None,
     }
@@ -94,6 +94,7 @@ async def get_all_apps(session: SessionDep, ofs: int = 0, limit: int = 15):
     for user in users:
         user_app = user.application    
         response.append({
+            "uid":user.uid,
             "first_name": user.first_name,
             "last_name": user.last_name,
             "email": user.email,
