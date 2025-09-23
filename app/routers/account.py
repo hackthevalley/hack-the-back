9ad583d01ef769740956960909bcee33351ee208
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-09-23 13:01:20 -0400

    Fix cooldown logic

--- app/routers/account.py
+++ app/routers/account.py
@@ -122,11 +122,12 @@ async def send_reset_password(user: PasswordReset, session: SessionDep):
         last_sent = selected_user.last_password_reset_request
         if last_sent.tzinfo is None:
             last_sent = last_sent.replace(tzinfo=timezone.utc)
-        if now - selected_user.last_password_reset_request < cooldown:
+        if now - last_sent < cooldown:  # <-- use last_sent here
             raise HTTPException(
                 status_code=status.HTTP_429_TOO_MANY_REQUESTS,
                 detail="Sent too many emails, please wait before requesting another password reset email.",
             )
+
     selected_user.last_password_reset_request = now
     session.add(selected_user)
     session.commit()
