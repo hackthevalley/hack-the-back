9964311a77467ab0158325d7082b269431e65361
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-09-30 17:47:05 -0400

    Add bulk_email_send route + hacker_package

--- app/models/requests.py
+++ app/models/requests.py
@@ -1,5 +1,19 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
+from app.models.forms import (
+    StatusEnum,
+)
 
 
 class UIDRequest(BaseModel):
     uid: str
+
+
+class BulkEmailRequest(BaseModel):
+    template_path: str
+    status: StatusEnum
+    subject: str
+    text_body: str
+    context: Optional[dict] = {}
