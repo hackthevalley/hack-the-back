f5eccdff0e3a742743525b3fe114bb55e6e53345
Conrad Mo <conradmo78@hotmail.com>
2025-09-30 16:02:40 -0400

    Fix scanning and food tracking (#85)
    * Fix scanning and food tracking
    
    * Add login logic
    
    * Refactor models
    
    * Add walkin logic

--- app/routers/admin/__init__.py
+++ app/routers/admin/__init__.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 
 from app.routers.admin.account import router as account
+from app.routers.admin.food import router as food
 from app.routers.admin.forms import router as forms
+from app.routers.admin.qr import router as qr
 
 router = APIRouter()
 
 router.include_router(account, prefix="/account")
 router.include_router(forms, prefix="/forms")
+router.include_router(qr, prefix="/qr")
+router.include_router(food, prefix="/food")
