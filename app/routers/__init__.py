b1cbc3ab74702c49670ff5fe057b06035bd2b002
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-10-02 13:47:13 -0400

    Add volunteer role

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -7,6 +7,7 @@ from app.routers.account import router as account
 from app.routers.admin import router as admin
 from app.routers.forms import router as forms
 from app.routers.meal import router as meal
+from app.routers.volunteer import router as volunteer
 from app.utils import decode_jwt
 
 router = APIRouter()
@@ -22,9 +23,25 @@ def is_admin(
     return True
 
 
+def is_volunteer(
+    token_data: Annotated[TokenData, Security(decode_jwt)],
+) -> bool:
+    if "admin" not in token_data.scopes and "volunteer" not in token_data.scopes:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User does not permission"
+        )
+    return True
+
+
 router.include_router(account, prefix="/account", tags=["account"])
 router.include_router(forms, prefix="/forms", tags=["forms"])
 router.include_router(
     admin, prefix="/admin", tags=["admin"], dependencies=[Depends(is_admin)]
 )
 router.include_router(meal, prefix="/meal", tags=["food-tracking"])
+router.include_router(
+    volunteer,
+    prefix="/volunteer",
+    tags=["volunteer"],
+    dependencies=[Depends(is_volunteer)],
+)
